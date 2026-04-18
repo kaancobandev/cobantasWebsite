48456c69e52738792ed9966274b58f460da3c269
kaan <kaaan3452@gmail.com>
2026-04-18 13:48:27 +0300

    fix logo code

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ export default function App() {
             {/* Logo */}
             <a href="#home" className="flex items-center gap-3 group">
               {/* Kendi logonuz public klasöründeki logo.png dosyasından buraya gelecektir */}
-              <img src="/çobantaşLogo.png" alt="Çobantaş Logo" className="h-10 w-auto object-contain" />
+              <img src="/çobantaşLogo.jpeg" alt="Çobantaş Logo" className="h-10 w-auto object-contain" />
               
               {/* NOT: Eğer logonuzun içinde ÇOBANTAŞ yazısı zaten varsa, aşağıdaki <div className="flex flex-col">...</div> bloğunu silebilirsiniz */}
               <div className="flex flex-col">
@@ -381,7 +381,7 @@ export default function App() {
             <div>
               <div className="flex items-center gap-3 mb-6 opacity-90">
                 {/* Alt bilgi için açık renkli logo versiyonu logo-beyaz.png */}
-                <img src="/çobantaşLogo.png" alt="Çobantaş Logo" className="h-8 w-auto object-contain" />
+                <img src="/çobantaşLogo.jpeg" alt="Çobantaş Logo" className="h-8 w-auto object-contain" />
                 <span className="text-2xl font-bold tracking-tighter text-white">
                   ÇOBANTAŞ
                 </span>
